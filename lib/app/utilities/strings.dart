1e4c8e734461ffc570b6c287cf198639bdf545f9
Ishaque <119050760+ishaque-mobikasa@users.noreply.github.com>
2024-03-26 10:50:15 +0530

    small change

--- lib/app/utilities/strings.dart
+++ lib/app/utilities/strings.dart
@@ -1,7 +1,9 @@
-String apiKey = "Bearer YourApiKeyHere";
+String apiKey = "Your API KEY HERE";
 
 class UrlStrings {
   static String baseUrl = "https://api.github.com/users/";
   static String getRepo({required String userName}) => "$userName/repos";
-  static Map<String, String> gitHubTokenHeader = {'Authorization': apiKey};
+  static Map<String, String> gitHubTokenHeader = {
+    'Authorization': 'Bearer $apiKey'
+  };
 }
